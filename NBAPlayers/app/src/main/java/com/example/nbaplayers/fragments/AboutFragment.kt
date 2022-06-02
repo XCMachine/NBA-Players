0079e47b4b67f6df43b116a883f12e02add9c71f
GeorgiTasev <georgi.tasev12@gmail.com>
2022-06-02 12:29:30 +0300

    Update the AboutFragment.kt and added the formatted version name, also added Glide library for the image view.

--- NBAPlayers/app/src/main/java/com/example/nbaplayers/fragments/AboutFragment.kt
+++ NBAPlayers/app/src/main/java/com/example/nbaplayers/fragments/AboutFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.example.nbaplayers.BuildConfig
+import com.example.nbaplayers.R
 import com.example.nbaplayers.databinding.FragmentAboutBinding
 
 class AboutFragment : Fragment() {
@@ -21,5 +24,16 @@ class AboutFragment : Fragment() {
     ): View {
         binding = FragmentAboutBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.versionText.text = BuildConfig.VERSION_NAME
+
+        Glide.with(this)
+            .load(R.drawable.nba_logo)
+            .centerCrop()
+            .into(binding.logoImageView)
     }
 }
